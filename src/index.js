52a151bfa67afe715b87832202bf707c67679936
shylahchiera <shylah.chiera@gmail.com>
2020-07-22 15:25:59 -0400

    price calculator

--- src/index.js
+++ src/index.js
@@ -12,11 +12,14 @@ function addProducts() {
     var productId = value.id;
     productHeader.innerHTML = productType + ' ' + productId;
     productEl.appendChild(productHeader)
-
+    var productPriceEl = document.createElement('p');
+    var productPrice = value.price.toFixed(2);
+    productPriceEl.innerHTML = '$' + productPrice;
+    productEl.appendChild(productPriceEl);
     var productOptionsEl = document.createElement('ul');
     $.each(value.options, function( key, value ) {
       var productOptionEl = document.createElement('li')
-      
+
       var productOptionTypeEl = document.createElement('strong')
       var productOptionType = key;
       productOptionTypeEl.textContent = productOptionType;
@@ -39,18 +42,40 @@ function addProducts() {
     addToCartBtn.addEventListener('click', () => {
       event.target.textContent = 'In Cart'
       event.target.classList += ' added'
+      event.target.disabled = true;
       var product = event.target.parentElement;
       var header = product.firstChild;
+      var price = header.nextSibling;
 
       var cartItems = $('.cart-items');
-      var cartItemEl = document.createElement('li')
-      cartItemEl.textContent = header.textContent;
+      var cartItemEl = document.createElement('li');
+      cartItemEl.style.display = 'flex';
+      cartItemEl.style.flexDirection = 'row';
+      cartItemEl.style.alignItems = 'center';
+      cartItemEl.style.justifyContent = 'space-between';
+      var cartItemHeaderEl = document.createElement('p');
+      var cartItemPriceEl = document.createElement('p');
+      cartItemHeaderEl.textContent = header.textContent;
+      cartItemPriceEl.textContent = price.textContent;
+      cartItemEl.appendChild(cartItemHeaderEl);
+      cartItemEl.appendChild(cartItemPriceEl);
+      $(price).addClass('item-price');
 
       cartItems.append(cartItemEl)
+
+      var total = 0;
+      $('.item-price').each(function() {
+        var price = parseInt(this.innerHTML.replace(/\$/g, ''));
+        total += price
+      })
+      var totalEl = document.getElementsByClassName('cart-total')[0];
+      totalEl.textContent = total;
     })
+
     productEl.appendChild(addToCartBtn)
 
     $('.items').append(productEl)
+
   });
 }
 
