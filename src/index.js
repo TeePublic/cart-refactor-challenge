9f13ddaba5ec01e09793c73ca9467c9a73f9afdd
Emmanuel Genard <edgenard@gmail.com>
2020-07-24 12:15:08 -0400

    Emmanuel Genard test

--- src/index.js
+++ src/index.js
@@ -1,87 +1,139 @@
-import './style.css';
-import $ from "jquery";
+import './style.css'
+import $ from 'jquery'
 import Products from './products.json'
 
-function addProducts() {
-  $.each(Products, function( index, value ) {
-    var productEl = document.createElement('div');
-    productEl.classList.add('item')
+// Todo
+//  - Replacing var with const and let
+//  - Extract createProductElement
+//  - Extract addToCart
+//  - Separate out styles from generating html elements
+//  - Create classes/components for CreateProduct, CreateCartItem etc
+//  - Use document.querySelector instead of Jquery to find elements
+//  - Use lodash for iteration
 
-    var productHeader = document.createElement('h3');
-    var productType = value.product_type;
-    var productId = value.id;
-    productHeader.innerHTML = productType + ' ' + productId;
-    productEl.appendChild(productHeader)
-    var productPriceEl = document.createElement('p');
-    var productPrice = value.price.toFixed(2);
-    productPriceEl.innerHTML = '$' + productPrice;
-    productEl.appendChild(productPriceEl);
-    var productOptionsEl = document.createElement('ul');
-    $.each(value.options, function( key, value ) {
-      var productOptionEl = document.createElement('li')
+function addProducts () {
+  $.each(Products, function (index, value) {
+    // Iterate through products.json and create a product element with options
+    const productEl = createProductElement(value)
 
-      var productOptionTypeEl = document.createElement('strong')
-      var productOptionType = key;
-      productOptionTypeEl.textContent = productOptionType;
-      productOptionsEl.appendChild(productOptionTypeEl)
+    productEl.appendChild(createProductHeader(value))
 
-      var productOptionValueEl = document.createElement('span')
-      var productOptionValue = value;
-      productOptionValueEl.textContent = ': ' + productOptionValue;
+    var productPriceEl = createProductPrice(value)
 
-      productOptionEl.appendChild(productOptionTypeEl)
-      productOptionEl.appendChild(productOptionValueEl)
-      productOptionsEl.appendChild(productOptionEl)
-    });
+    productEl.appendChild(productPriceEl)
+
+    var productOptionsEl = createProductOptions(value)
+
     productEl.appendChild(productOptionsEl)
 
-    var addToCartBtn = document.createElement('button')
-    addToCartBtn.textContent = 'Add to Cart'
-    addToCartBtn.classList.add('add-to-cart')
+    var addToCartBtn = createAddToCart()
 
-    addToCartBtn.addEventListener('click', () => {
-      event.target.textContent = 'In Cart'
-      event.target.classList += ' added'
-      event.target.disabled = true;
-      var product = event.target.parentElement;
-      var header = product.firstChild;
-      var price = header.nextSibling;
-
-      var cartItems = $('.cart-items');
-      var cartItemEl = document.createElement('li');
-      cartItemEl.style.display = 'flex';
-      cartItemEl.style.flexDirection = 'row';
-      cartItemEl.style.alignItems = 'center';
-      cartItemEl.style.justifyContent = 'space-between';
-      cartItemEl.style.textTransform = 'capitalize';
-      var cartItemHeaderEl = document.createElement('p');
-      var cartItemPriceEl = document.createElement('p');
-      cartItemHeaderEl.textContent = header.textContent;
-      cartItemPriceEl.textContent = price.textContent;
-      cartItemEl.appendChild(cartItemHeaderEl);
-      cartItemEl.appendChild(cartItemPriceEl);
-      $(price).addClass('item-price');
-
-      cartItems.append(cartItemEl)
-
-      var total = 0;
-      $('.item-price').each(function() {
-        var price = parseInt(this.innerHTML.replace(/\$/g, ''));
-        total += price
-      })
-      var totalEl = document.getElementsByClassName('cart-total')[0];
-      totalEl.textContent = '$' + total.toFixed(2);
-    })
+    addListenerToCartBtn(addToCartBtn)
 
     productEl.appendChild(addToCartBtn)
 
     $('.items').append(productEl)
+  })
+}
 
-  });
+// map over products create
+
+function addListenerToCartBtn (addToCartBtn) {
+  addToCartBtn.addEventListener('click', (event) => {
+    disableAddToCartBtn(event)
+
+    const header = event.target.parentElement.firstChild
+    const price = header.nextSibling
+
+    const cartItems = $('.cart-items')
+    const cartItemEl = createCartItemEl(header, price)
+
+    cartItems.append(cartItemEl)
+
+    let total = 0
+    document.querySelectorAll('.item-price').forEach((value, key, parent) => {
+      total += parseInt(value.innerHTML.replace(/\$/g, ''))
+    })
+
+    const totalEl = document.getElementsByClassName('cart-total')[0]
+    totalEl.textContent = '$' + total.toFixed(2)
+  })
+}
+
+function disableAddToCartBtn (event) {
+  event.target.textContent = 'In Cart'
+  event.target.classList += ' added'
+  event.target.disabled = true
+}
+
+function createCartItemEl (header, price) {
+  var cartItemEl = document.createElement('li')
+  cartItemEl.style.display = 'flex'
+  cartItemEl.style.flexDirection = 'row'
+  cartItemEl.style.alignItems = 'center'
+  cartItemEl.style.justifyContent = 'space-between'
+  cartItemEl.style.textTransform = 'capitalize'
+  var cartItemHeaderEl = document.createElement('p')
+  var cartItemPriceEl = document.createElement('p')
+  cartItemHeaderEl.textContent = header.textContent
+  cartItemPriceEl.textContent = price.textContent
+  cartItemEl.appendChild(cartItemHeaderEl)
+  cartItemEl.appendChild(cartItemPriceEl)
+  $(price).addClass('item-price')
+  return cartItemEl
+}
+
+function createAddToCart () {
+  var addToCartBtn = document.createElement('button')
+  addToCartBtn.textContent = 'Add to Cart'
+  addToCartBtn.classList.add('add-to-cart')
+  return addToCartBtn
+}
+
+function createProductOptions (value) {
+  var productOptionsEl = document.createElement('ul')
+  $.each(value.options, function (key, value) {
+    var productOptionEl = document.createElement('li')
+
+    var productOptionTypeEl = document.createElement('strong')
+    var productOptionType = key
+    productOptionTypeEl.textContent = productOptionType
+    productOptionsEl.appendChild(productOptionTypeEl)
+
+    var productOptionValueEl = document.createElement('span')
+    var productOptionValue = value
+    productOptionValueEl.textContent = ': ' + productOptionValue
+
+    productOptionEl.appendChild(productOptionTypeEl)
+    productOptionEl.appendChild(productOptionValueEl)
+    productOptionsEl.appendChild(productOptionEl)
+  })
+  return productOptionsEl
+}
+
+function createProductPrice (value) {
+  const productPriceEl = document.createElement('p')
+  const productPrice = value.price.toFixed(2)
+  productPriceEl.innerHTML = '$' + productPrice
+  return productPriceEl
+}
+
+function createProductHeader (value) {
+  const productHeader = document.createElement('h3')
+  const productType = value.product_type
+  const productId = value.id
+  productHeader.innerHTML = productType + ' ' + productId
+  return productHeader
+}
+
+function createProductElement (product) {
+  const productEl = document.createElement('div')
+  productEl.classList.add('item')
+  return productEl
 }
 
 // document.body.appendChild(products());
 
 document.addEventListener('DOMContentLoaded', () => {
   addProducts()
-});
+})
